9fd78ff244461ec32fa9951f0a5beb67deabf41a
ayushicodes <raiayushi12911@gmail.com>
2021-06-14 21:32:48 +0530

    fixed eventListener

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-var btntranslate = document.querySelector("#btn-translate");
-var txtarea = document.querySelector("#txt-input");
-console.log(txtarea);
-function clickHandler() {
-    console.log("clicked");
-    console.log("input", txtarea.value);
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverUrl + "?" + "text=" + text
 }
-btntranslate.addEventListener("click", clickHandler);
+
+function errorHandler(error) {
+    console.log("error occured", error);
+
+}
+
+function clickHandler() {
+    var txtarea = document.querySelector("#txt-input");
+    var outputDiv = document.querySelector("#output");
+
+    var inputText = txtarea.value
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            console.log('json', json);
+            var translatedText = json.contents.translated;
+            outputDiv.innerHTML = translatedText
+        })
+        .catch(errorHandler)
+}
+
+window.onload = function () {
+    var btntranslate = document.querySelector("#btn-translate");
+
+    if (btntranslate) {
+        btntranslate.addEventListener("click", clickHandler);
+    }
+
+}
